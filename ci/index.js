7d39a3ca61138d90e375ad79fb50e4e6e1b55c1d
Thiago Bueno <thiago.bueno@optibus.com>
2024-11-22 11:16:05 -0300

    Call jenkins at the end of the workflow

---
+++ ci/index.js
@@ -0,0 +1,34 @@
+const Jenkins = require('jenkins');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const userName = process.env.CIRCLECI_JENKINS_USER;
+const token = process.env.CIRCLECI_JENKINS_TOKEN;
+
+const url = `https://${userName}:${token}@jenkins-ci.optibus.com`;
+
+const jenkins = new Jenkins({
+  baseUrl: url,
+});
+
+const jobName = process.argv[2];
+
+(async () => {
+  try {
+    console.log("Triggering job:", jobName);
+    const queueItem = await jenkins.job.build({
+      name: jobName,
+      // TODO: remove the branch parameter, since it will come in the jobName
+      parameters: { BRANCH_NAME: "main" },
+    });
+    console.log("Queue item:", queueItem);
+
+    const info = await await jenkins.queue.item(queueItem);
+    console.log(`Job triggered in the following URL: ${info.task.url}`);
+
+  } catch (err) {
+    console.error('Error:', err);
+    process.exit(1);
+  }
+})()
